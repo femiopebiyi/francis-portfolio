4cb396bde54de8a4b5d810b4771bc8cbfcf83be8
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-26 20:51:07 +0100

    download resume button

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const downloadCv =  document.querySelector('.download-cv');
+
+downloadCv.addEventListener ('click', () => {
+    const a = document.createElement("a");
+    a.href = "resume.PDF"; 
+    a.download = "resume.PDF";
+
+    a.click();
+})
